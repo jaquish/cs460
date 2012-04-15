f384223b07cd9f4643a28ad1b4d757aa421c0dda
Zachary Jaquish <zach.jaquish@gmail.com>
2012-04-14 17:47:23 -0700

    added my_types.h

---
+++ my_types.h
@@ -0,0 +1,9 @@
+#ifndef MY_TYPES_H
+#define MY_TYPES_H
+
+// these are correct for my mac and Mint 64-bit Lisa 12. YMMV
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+#endif
